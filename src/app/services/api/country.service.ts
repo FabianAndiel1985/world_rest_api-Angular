a14f4069a4d72e240d04b2a57be28a2d22767d60
Fabian Andiel <fabian.andiel@gmail.com>
2025-01-07 13:57:02 +0100

    adds country service

---
+++ src/app/services/api/country.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CountryService {
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor() {}
+}
