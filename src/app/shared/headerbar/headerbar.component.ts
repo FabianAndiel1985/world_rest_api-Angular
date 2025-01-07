1cc512c6f6dd47cb0f433886e884affc646499a3
Fabian Andiel <fabian.andiel@gmail.com>
2025-01-07 10:47:36 +0100

    adds more functionality to headerbar

--- src/app/shared/headerbar/headerbar.component.ts
+++ src/app/shared/headerbar/headerbar.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-headerbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './headerbar.component.html',
   styleUrl: './headerbar.component.scss',
 })
 export class HeaderbarComponent {
   isBrightTheme: boolean = true;
+  faMoon = faMoon;
 
   toggleTheme() {
     this.isBrightTheme = !this.isBrightTheme;
