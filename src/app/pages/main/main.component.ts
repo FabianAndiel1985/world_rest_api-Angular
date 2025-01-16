dfc2c604d984b0004999c40f4179ee5a36a8ea9c
Fabian Andiel <fabian.andiel@gmail.com>
2025-01-16 14:51:37 +0100

    sets up service and first request

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardComponent } from '../../shared/card/card.component';
+import { CountryService } from '../../services/api/country.service';
+import { catchError, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent {}
+export class MainComponent implements OnInit {
+  countries: any[] = [];
+
+  constructor(private countryService: CountryService) {}
+
+  ngOnInit(): void {
+    this.countryService
+      .getAllCountries()
+      .pipe(
+        catchError((error: any) => {
+          console.error('An error occurred:', error);
+          return of([]);
+        })
+      )
+      .subscribe((countries) => {
+        this.countries = countries;
+      });
+  }
+}
--- src/app/services/api/country.service.ts
+++ src/app/services/api/country.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +8,9 @@ import { Injectable } from '@angular/core';
 export class CountryService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getAllCountries(): Observable<any> {
+    return this.http.get(this.apiUrl + '/all');
+  }
 }
