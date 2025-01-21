ecbafb34eba8e4bb1a745bebe4ee274446b6e3cb
Fabian Andiel <fabian.andiel@gmail.com>
2025-01-21 14:54:04 +0100

    adds cards to main

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-
+  @Input() imgSrc: string = '';
+  @Input() name: string = '';
+  @Input() population: number = 0;
+  @Input() region: string = '';
+  @Input() capital: string[] = [];
 }
